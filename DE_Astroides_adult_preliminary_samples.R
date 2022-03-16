f5f42949686183ffb89285c5050db129c0c97e6d
Marc Meynadier <mmeynadier@fera.obs-vlfr.fr>
2022-03-16 15:29:05 +0100

    new version

--- DE_Astroides_adult_preliminary_samples.R
+++ DE_Astroides_adult_preliminary_samples.R
@@ -3,7 +3,6 @@
 # Preliminary samples 
 
 # Packages and dependence
-
 packageCheckClassic <- function(x){
   for( i in x ){
     #  require returns TRUE invisibly if it was able to load package
@@ -27,8 +26,10 @@ library('ashr')
 library('EnhancedVolcano')
 
 # Working environment 
-
 scriptPath<-dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(scriptPath)
+samples<-read.table('tximport_design_preliminarySamples.txt',header=T)
+tx2gene<-read.table('tx2gene',header=T)
 scriptPath <- sub("/[^/]+$", "", scriptPath)
 dataPath<-'/data/net/5_kallisto/larvaeAdultTranscriptome/adult/nov2016'
 outputPath<-paste(scriptPath,'/output/DESeq2/1_preliminarySamples/',sep='')
@@ -36,32 +37,22 @@ wdPath<-paste(scriptPath,dataPath,sep='')
 setwd(wdPath)
 
 # Data importation - txImport
-
-samples<-read.table('tximport_design_preliminarySamples.txt',header=T)
-tx2gene<-read.table('tx2gene',header=T)
-
 files<-paste0(samples$sample,'.tsv')
-
 names(files)<-samples$sample
-
 txi<-tximport(files = files,type='kallisto',tx2gene = tx2gene)
-
 names(txi)
-
 head(txi$counts)
-
-dds<-DESeqDataSetFromTximport(txi,colData=samples,design= ~condition)
+dds<-DESeqDataSetFromTximport(txi,colData=samples,design= ~site)
 
 # pre-filtering
 keep <- rowSums(counts(dds)) >= 10 
 dds <- dds[keep,]
 
 # Differential expression analysis
-
 dds<-DESeq(dds)
 cbind(resultsNames(dds))
-res_gm_pv<-results(dds, contrast=c("condition","gm","pv"), alpha = 0.05)
-res_gm_sa<-results(dds, contrast=c("condition","gm","sa"), alpha = 0.05)
+res_gm_pv<-results(dds, contrast=c("site","gm","pv"), alpha = 0.05)
+res_gm_sa<-results(dds, contrast=c("site","gm","sa"), alpha = 0.05)
 summary(res_gm_pv)
 summary(res_gm_sa)
 
@@ -70,51 +61,16 @@ summary(res_gm_sa)
 # Results gm VS pv
 
 #MA-plot
-png(paste(outputPath,'DGE_MA_plot_adult_adultTranscriptome_nov2016_gm_VS_pv.png',sep=''), width=7, height=5, units = "in", res = 300)
-resLFC = lfcShrink(dds, contrast=c("condition","gm","pv"), 
+png(paste(outputPath,'DGE_MA-plot_adult_preliminarySamples_gm_VS_pv.png',sep=''), width=7, height=5, units = "in", res = 300)
+resLFC = lfcShrink(dds, contrast=c("site","gm","pv"), 
                    type="ashr")
 plotMA(resLFC, alpha = 0.05, ylim=c(-25,25), main = "MA-plot for the shrunken log2 fold changes")
-dev.off()
-
-# Principal Component Analysis
-
-# rlog transformation
-rld = rlog(dds)
-
-pcaData = plotPCA(rld, intgroup="condition", 
-                  returnData=TRUE)
-percentVar = round(100 * attr(pcaData, "percentVar"))
-png(paste(outputPath,'DGE_PCA_rlog_adult_adultTranscriptome_nov2016_gm_VS_pv.png',sep=''), width=7, height=7, units = "in", res = 300)
-ggplot(pcaData, aes(PC1, PC2, colour = condition)) + 
-  geom_point(size = 2) + theme_bw() + 
-  scale_color_manual(values = c("blue", "red","green")) +
-  geom_text_repel(aes(label = condition), nudge_x = -1, nudge_y = 0.2, size = 3) +
-  ggtitle("Principal Component Analysis (PCA)", subtitle = "rlog transformation") +
-  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-  ylab(paste0("PC2: ",percentVar[2],"% variance"))
-dev.off()
-
-# vst transformation
-vsd = vst(dds)
-
-pcaData = plotPCA(vsd, intgroup="condition", 
-                  returnData=TRUE)
-percentVar = round(100 * attr(pcaData, "percentVar"))
-png(paste(outputPath,'DGE_PCA_vst_adult_adultTranscriptome_nov2016_gm_VS_pv.png',sep=''), width=7, height=7, units = "in", res = 300)
-vstPlot<-ggplot(pcaData, aes(PC1, PC2, colour = condition)) + 
-  geom_point(size = 2) + theme_bw() + 
-  scale_color_manual(values = c("blue", "red","green")) +
-  geom_text_repel(aes(label = condition), nudge_x = -1, nudge_y = 0.2, size = 3) +
-  ggtitle("Principal Component Analysis (PCA)", subtitle = "vst transformation") +
-  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-  ylab(paste0("PC2: ",percentVar[2],"% variance"))
-vstPlot
 dev.off()
 
 # Volcano plot
 pCutoff = 0.05
 FCcutoff = 1.0
-png(paste(outputPath,'DGE_VolcanoPlots_adult_adultTranscriptome_nov2016_gm_VS_pv.png',sep=''), width=7, height=7, units = "in", res = 300)
+png(paste(outputPath,'DGE_volcanoPlot_adult_preliminarySamples_VS_pv.png',sep=''), width=7, height=7, units = "in", res = 300)
 EnhancedVolcano(data.frame(res_gm_pv), lab = rownames(data.frame(res_gm_pv)), x = 'log2FoldChange', y = 'padj',
                     xlab = bquote(~Log[2]~ 'fold change'), ylab = bquote(~-Log[10]~adjusted~italic(P)),
                     pCutoff = pCutoff, FCcutoff = FCcutoff, pointSize = 1.0, labSize = 2.0,
@@ -127,54 +83,15 @@ dev.off()
 # Results gm VS sa
 
 #MA-plot
-png(paste(outputPath,'DGE_MA-plot_adult_adultTranscriptome_nov2016_gm_VS_sa.png',sep=''), width=7, height=5, units = "in", res = 300)
-resLFC = lfcShrink(dds, contrast=c("condition","gm","sa"), 
+png(paste(outputPath,'DGE_MA-plot_adult_preliminarySamples_gm_VS_sa.png',sep=''), width=7, height=5, units = "in", res = 300)
+resLFC = lfcShrink(dds, contrast=c("site","gm","sa"), 
                    type="ashr")
 plotMA(resLFC, alpha = 0.05, ylim=c(-25,25), 
        main = "MA-plot for the shrunken log2 fold changes")
 dev.off()
 
-# Principal Component Analysis
-
-# rlog transformation
-rld = rlog(dds)
-
-pcaData = plotPCA(rld, intgroup="condition", 
-                  returnData=TRUE)
-percentVar = round(100 * attr(pcaData, "percentVar"))
-
-png(paste(outputPath,'DGE_PCA-rlog_adult_adultTranscriptome_nov2016_gm_sa.png',sep=''), width=7, height=7, units = "in", res = 300)
-ggplot(pcaData, aes(PC1, PC2, colour = condition)) + 
-  geom_point(size = 2) + theme_bw() + 
-  scale_color_manual(values = c("blue", "red","green")) +
-  geom_text_repel(aes(label = condition), nudge_x = -1, nudge_y = 0.2, size = 3) +
-  ggtitle("Principal Component Analysis (PCA)", subtitle = "rlog transformation") +
-  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-  ylab(paste0("PC2: ",percentVar[2],"% variance"))
-dev.off()
-
-# vst transformation
-vsd = vst(dds)
-
-pcaData = plotPCA(vsd, intgroup="condition", 
-                  returnData=TRUE)
-percentVar = round(100 * attr(pcaData, "percentVar"))
-
-png(paste(outputPath,'DGE_PCA-vst_adult_adultTranscriptome_nov2016_gm_VS_sa.png',sep=''), width=7, height=7, units = "in", res = 300)
-ggplot(pcaData, aes(PC1, PC2, colour = condition)) + 
-  geom_point(size = 2) + theme_bw() + 
-  scale_color_manual(values = c("blue", "red","green")) +
-  geom_text_repel(aes(label = condition), nudge_x = -1, nudge_y = 0.2, size = 3) +
-  ggtitle("Principal Component Analysis (PCA)", subtitle = "vst transformation") +
-  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-  ylab(paste0("PC2: ",percentVar[2],"% variance"))
-dev.off()
-
 # Volcano plot
-pCutoff = 0.05
-FCcutoff = 1.0
-
-png(paste(outputPath,'DGE_VolcanoPlots_adult_adultTranscriptome_nov2016_gm_VS_sa.png',sep=''), width=7, height=7, units = "in", res = 300)
+png(paste(outputPath,'DGE_volcanoPlot_adult_preliminarySamples_gm_VS_sa.png',sep=''), width=7, height=7, units = "in", res = 300)
 EnhancedVolcano(data.frame(res_gm_sa), lab = rownames(data.frame(res_gm_sa)), x = 'log2FoldChange', y = 'padj',
                     xlab = bquote(~Log[2]~ 'fold change'), ylab = bquote(~-Log[10]~adjusted~italic(P)),
                     pCutoff = pCutoff, FCcutoff = FCcutoff, pointSize = 1.0, labSize = 2.0,
@@ -184,19 +101,65 @@ EnhancedVolcano(data.frame(res_gm_sa), lab = rownames(data.frame(res_gm_sa)), x 
                     legendPosition = 'bottom', legendLabSize = 14, legendIconSize = 5.0)
 dev.off()
 
+# Principal Component Analysis
+
+# rlog transformation
+rld = rlog(dds)
+
+pcaData = plotPCA(rld, intgroup="site", 
+                  returnData=TRUE)
+percentVar = round(100 * attr(pcaData, "percentVar"))
+
+png(paste(outputPath,'DGE_PCA_rlog_adult_preliminarySamples.png',sep=''), width=7, height=7, units = "in", res = 300)
+ggplot(pcaData, aes(PC1, PC2, colour = site)) + 
+  geom_point(size = 2) + theme_bw() + 
+  scale_color_manual(values = c("blue", "red","green")) +
+  geom_text_repel(aes(label = site), nudge_x = -1, nudge_y = 0.2, size = 3) +
+  ggtitle("Principal Component Analysis (PCA)", subtitle = "rlog transformation") +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance"))
+dev.off()
+
+# vst transformation
+vsd = vst(dds)
+
+pcaData = plotPCA(vsd, intgroup="site", 
+                  returnData=TRUE)
+percentVar = round(100 * attr(pcaData, "percentVar"))
+
+png(paste(outputPath,'DGE_PCA_vst_adult_preliminarySamples.png',sep=''), width=7, height=7, units = "in", res = 300)
+ggplot(pcaData, aes(PC1, PC2, colour = site)) + 
+  geom_point(size = 2) + theme_bw() + 
+  scale_color_manual(values = c("blue", "red","green")) +
+  geom_text_repel(aes(label = site), nudge_x = -1, nudge_y = 0.2, size = 3) +
+  ggtitle("Principal Component Analysis (PCA)", subtitle = "vst transformation") +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance"))
+dev.off()
 
 # heatmap
 
-topVarGenes <- head(order(rowVars(assay(vsd)), decreasing=TRUE), 100 )
-heatmap.2(assay(vsd)[topVarGenes,], trace="none",scale="row", dendrogram="column", keysize=1, key.par = list(cex=0.5),
-           col=colorRampPalette(rev(brewer.pal(9,"RdBu")))(255), cexRow=0.5, cexCol=0.7, labCol=F,
-           ColSideColors=c(gm="gray", pv="green", sa="yellow")[
-             colData(vsd)$condition],xlab="sampling sites",ylab="genes",margins = c(2, 11),
-          lmat=rbind(c(5, 4, 2), c(6, 1, 3)), lhei=c(2.5, 5), lwid=c(1, 10, 1))
+# rlog transformation
+topVarGenesRld <- head(order(rowVars(assay(rld)), decreasing=TRUE), 100 )
+png(paste(outputPath,'DGE_heatmap_rlog_adult_preliminarySamples.png',sep=''), width=20, height=20, units = "in", res = 300)
+heatmap.2(assay(rld)[topVarGenesRld,], trace="none",scale="row",keysize=1, key.par = list(cex=0.5),
+          col=colorRampPalette(rev(brewer.pal(9,"RdBu")))(255), cexRow=0.5, cexCol=0.7, labCol=F,
+          main = "Differentially expressed genes\nin preliminary samples (rlog transformation)",
+          ColSideColors=c(gm="gray", pv="green", sa="yellow")[
+            colData(vsd)$site],xlab="sampling sites",ylab="genes",margins = c(2, 11))
+dev.off()
 
+# vst transformation
+topVarGenesVsd <- head(order(rowVars(assay(vsd)), decreasing=TRUE), 100 )
+png(paste(outputPath,'DGE_heatmap_vst_adult_preliminarySamples.png',sep=''), width=7, height=7, units = "in", res = 300)
+heatmap.2(assay(vsd)[topVarGenesVsd,], trace="none",scale="row",keysize=1, key.par = list(cex=0.5),
+          col=colorRampPalette(rev(brewer.pal(9,"RdBu")))(255), cexRow=0.5, cexCol=0.7, labCol=F,
+          main = "Differentially expressed genes\nin preliminary samples (vst transformation)",
+          ColSideColors=c(gm="gray", pv="green", sa="yellow")[
+            colData(vsd)$site],xlab="sampling sites",ylab="genes",margins = c(2, 11))
+dev.off()
 
 # Exporting results
-
 resOrdered_gm_pv <- res_gm_pv[order(res_gm_pv$pvalue),]
 resOrdered_gm_sa <- res_gm_sa[order(res_gm_sa$pvalue),]
 head(resOrdered_gm_pv)
@@ -204,7 +167,7 @@ head(resOrdered_gm_sa)
 
 resOrderedDF_gm_pv <- as.data.frame(resOrdered_gm_pv)
 resOrderedDF_gm_sa <- as.data.frame(resOrdered_gm_sa)
-write.csv(resOrderedDF_gm_pv, file = paste(scriptPath,'/data/net/6_deseq2/adult/1_preliminarySamples/DESeq2_results_adult_adultTranscriptome_nov2016_gm_VS_pv.csv',sep=''))
-write.csv(resOrderedDF_gm_sa, file = paste(scriptPath,'/data/net/6_deseq2/adult/1_preliminarySamples/DESeq2_results_adult_adultTranscriptome_nov2016_gm_VS_sa.csv',sep=''))
+write.csv(resOrderedDF_gm_pv, file = paste(scriptPath,'/data/net/6_deseq2/adult/1_preliminarySamples/DESeq2_results_adult_preliminarySamples_gm_VS_pv.csv',sep=''))
+write.csv(resOrderedDF_gm_sa, file = paste(scriptPath,'/data/net/6_deseq2/adult/1_preliminarySamples/DESeq2_results_adult_preliminarySamples_gm_VS_sa.csv',sep=''))
 
 sessionInfo()
