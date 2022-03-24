692d64a46d4771012f2c5460bea78b20cfbad38b
Marc Meynadier <mmeynadier@fera.obs-vlfr.fr>
2022-03-24 11:23:00 +0100

    last commit

--- expressedGeneMatching.py
+++ expressedGeneMatching.py
@@ -6,22 +6,20 @@ import os
 import glob
 import pandas as pd
 import numpy as np
+from functools import reduce
 
 threshold_pvalue=0.05
 
 os.chdir('../data/net/6_deseq2/adult') # Changing working directory to DESeq2 results
 path=os.getcwd()
-filenamesPreSamp = glob.glob(path + "/*preliminarySamples*.csv")
 filenamesTrueTransp = glob.glob(path + "/*trueTransplant*.csv")
 
-dfsPreSamp = [pd.read_csv(filename) for filename in filenamesPreSamp]
-for i in range(len(dfsPreSamp)):
-    dfsPreSamp[i]=dfsPreSamp[i].drop(dfsPreSamp[i][dfsPreSamp[i].padj>threshold_pvalue].index)
-    dfsPreSamp[i]=dfsPreSamp[i][dfsPreSamp[i]['padj'].notna()]
-concatPreSamp = pd.concat(dfsPreSamp, ignore_index=True)
+for filename in filenamesTrueTransp:
+    print(filename)
 
 dfsTrueTransp = [pd.read_csv(filename) for filename in filenamesTrueTransp]
 for i in range(len(dfsTrueTransp)):
+    dfsTrueTransp[i].rename(columns={ dfsTrueTransp[i].columns[0]: "gene" }, inplace = True)
     dfsTrueTransp[i]=dfsTrueTransp[i].drop(dfsTrueTransp[i][dfsTrueTransp[i].padj>threshold_pvalue].index)
     dfsTrueTransp[i]=dfsTrueTransp[i][dfsTrueTransp[i]['padj'].notna()]
 concatTrueTransp = pd.concat(dfsTrueTransp, ignore_index=True)
@@ -31,16 +29,9 @@ concatTrueTransp = pd.concat(dfsTrueTransp, ignore_index=True)
 #for index, row in concatPreSamp.iterrows():
 #  print(row[0])
 
-def comparingDataframe(df1,df2):
-    sharedGenes=[]
-    unsharedGenes=[]
-    for i, row in df1.iterrows():
-        for j, row2 in df2.iterrows():
-            if row[0]==row2[0]:
-                sharedGenes.append(row[0])
-    sharedGenes=np.unique(sharedGenes)
-    print("list of shared genes :",sharedGenes)
-    print("Number of shared genes :",len(sharedGenes))
-    return sharedGenes
-  
-comparingDataframe(concatPreSamp,concatTrueTransp)
+#output2=list(set(dfsTrueTransp[0].gene) & set(dfsTrueTransp[2].gene))
+output=list(reduce(set.intersection, map(set, [dfsTrueTransp[0].gene, dfsTrueTransp[2].gene])))
+for i in range(len(output)):
+    output[i]=output[i].replace('TRINITY_','')
+print(output) ; print(len(output))
+
