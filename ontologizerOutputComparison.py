3b16073e18f51b06f87eec8a5a30e2b41276269e
MarcMeynadier <marc.meynadier@gmx.fr>
2022-04-19 23:08:38 +0200

    working programm

--- ontologizerOutputComparison.py
+++ ontologizerOutputComparison.py
@@ -12,22 +12,46 @@ Marc Meynadier
 import os
 import sys
 from regex import F
-import wget
 import pandas as pd
-import csv
-from urllib.request import urlopen
+from functools import reduce
+
+
+def filenamesToDataframe(filenames):
+    print("Define your threshold value (usually 0.05)")
+    threshold_pvalue=float(input())
+    dfs = [pd.read_csv(filename,error_bad_lines=False,sep='\t') for filename in filenames]
+    for i in range(len(dfs)):
+        dfs[i]=dfs[i].drop(dfs[i][dfs[i].p>threshold_pvalue].index)
+    return dfs
 
 
 def getFilesNames():
-    threshold_pvalue=0.05
     os.chdir('../data/net/7_functionnalAnnotation/ontologizer/outputResults/codingDEG') # Changing working directory to DESeq2 results
     path=os.getcwd()
+    filesNamesClean=[]
+    filesNames = []
     for file in os.listdir(path):
         if file.endswith(".txt"):
-            curedFile = []
-            fileName = file.replace('.csv','')
-            csvFile = pd.read_csv(file,on_bad_lines='skip',sep='\t')
-            print(csvFile)
-            
+            filesNames.append(file)
+            fileName = file.split("results_",1)[1] ; fileName = fileName.split("-Parent",1)[0]
+            filesNamesClean.append(fileName)
+    dfs = filenamesToDataframe(filesNames)
+    print("\nList of output files from Ontologizer\n")
+    for i in range(len(filesNamesClean)):
+        filesNamesClean[i] = str(i+1) + " : " + filesNamesClean[i]
+        print(filesNamesClean[i]) 
+    print("\nHow many files do you want to compare ?\n")  
+    nbrFiles = int(input())
+    comparedFiles = []
+    print("\nWhich files do you want to compare ?\n")
+    for i in range(nbrFiles):
+        comparedFiles.append(dfs[int(input())-1])
+    outputDf = comparedFiles[0] 
+    for i in range(len(comparedFiles)):
+        try:
+            outputDf = outputDf.merge(comparedFiles[i+1],how='inner',on='ID')
+        except IndexError:
+            pass
+    print(outputDf)
 
 getFilesNames()
