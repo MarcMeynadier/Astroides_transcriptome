e516489810e881f4e0d5326978d20a9d4c7f0a79
MarcMeynadier <81370891+MarcMeynadier@users.noreply.github.com>
2022-04-29 15:58:24 +0200

    Delete analyseGO.R

--- analyseGO.R
+++
@@ -1,7 +0,0 @@
-# Working environment 
-scriptPath<-dirname(rstudioapi::getSourceEditorContext()$path)
-dataPath<-'../data/net/7_functionnalAnnotation'
-wdPath<-paste(scriptPath,dataPath,sep='/')
-setwd(wdPath)
-
-file = 
