40fb506d5050cec9ece7cef88395bb74db61badd
MarcMeynadier <81370891+MarcMeynadier@users.noreply.github.com>
2022-04-29 15:59:17 +0200

    Delete trinityFullTranscriptome.sh

--- trinityFullTranscriptome.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/scratch1/genomes/copley/src/trinityrnaseq-v2.13.2/Trinity --seqType fq --max_memory 128G --samples_file samples_file.txt --CPU 32 --output full_trinity --full_cleanup
-
-
